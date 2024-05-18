9ed4d9cda25c2046ff30e9ebeb9feb631e24b892
NhatVN <nhatdn96it@gmail.com>
2024-05-19 00:28:57 +0700

    fix bug UI

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -3,24 +3,15 @@ import "../css/not-found-page.css"
 
 const NotFoundPage = () => {
     return (
-        <section class="page-404">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="col-sm-10 col-sm-offset-1 text-center">
-                            <div class="four-zero-four-bg">
-                                <h1 class="text-center">404</h1>
-                            </div>
-
-                            <div class="contant-box-404">
-                                <p>The page you are looking for not avaible!</p>
-                                <Link to="/" className="link-404">Go to Home</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className="center">
+            <div className="four-zero-four-bg">
+                <h1 className="mb-0">404</h1>
             </div>
-        </section>
+            <div>
+                <p>The page you are looking for not avaible!</p>
+                <Link to="/" className="btn btn-success">Go to Home</Link>
+            </div>
+        </div>
     )
 };
 
